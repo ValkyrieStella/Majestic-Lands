67dca94986f0c7d59710c01890f8ba47d4293f4f
Koda_ <67247561+KodaTheGuy@users.noreply.github.com>
2021-01-13 11:58:19 -0800

    Update ranks.lua

--- smp/mods/plugins/ranks/ranks.lua
+++ smp/mods/plugins/ranks/ranks.lua
@@ -1,26 +1,26 @@
 -- ranks/ranks.lua
 
 ranks.register("owner", {
-	prefix = "[Owner]",
+	prefix = "Owner",
 	colour = {a = 255, r = 230, g = 33, b = 23},
 })
 
 ranks.register("south", {
-	prefix = "[South]",
+	prefix = "South",
 	colour = {a = 1, r = 0, g = 0, b = 255},
 })
 
 ranks.register("east", {
-	prefix = "[East]",
+	prefix = "East",
 	colour = {a = 1, r = 0, g = 0, b = 0},
 })
 
 ranks.register("west", {
-	prefix = "[West]",
+	prefix = "West",
 	colour = {a = 1, r = 0, g = 255, b = 0},
 })
 
 ranks.register("south", {
-	prefix = "[South]",
+	prefix = "South",
 	colour = {a = 1, r = 255, g = 0, b = 0},
 })
